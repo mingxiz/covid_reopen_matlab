9208c6ea965c8c3a0d803ffd0c534dd9ab2dd74c
Mingxi Zhu <48236042+mingxiz@users.noreply.github.com>
2020-04-28 15:08:55 -0700

    note on v_table_policy, to-do

--- generate_param_beta.m
+++ generate_param_beta.m
@@ -8,7 +8,7 @@ function parm_beta = generate_param_beta(n_age_strat, n_work_strat, param_epi)
         V(:,:,i)=V_temp((i-1)*size(V,2)+1:i*size(V,2),:);
     end
     v_table_nopolicy = sum(V,3);
-
+    % here assume same, should not be , need more work
     v_table_policy = v_table_nopolicy;
     
     parm_beta = covid_open_beta(n_age_strat, n_work_strat, v_table_nopolicy, v_table_policy, param_epi);
