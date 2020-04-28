fbdc45ce4b955ce52676e3e8f2f6b0db23ea8f85
Mingxi Zhu <48236042+mingxiz@users.noreply.github.com>
2020-04-28 15:15:01 -0700

    Update generate_param_policy.m

--- generate_param_policy.m
+++ generate_param_policy.m
@@ -1,5 +1,10 @@
 function param_policy = generate_param_policy
 
+%  SEIR Model for COVID-19 reopenning project
+%  Written for MATLAB_R2019b
+%  Copyright (C) 2020
+%     Mingxi Zhu <mingxiz@stanford.edu>
+
     param_policy.e = 1;
     % percentage of people under policy control (could be work X age specific)
     % each entry is in (0, 1)
