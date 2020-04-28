4deb92e92677706a6156e1f336179bc00db463c3
Mingxi Zhu <48236042+mingxiz@users.noreply.github.com>
2020-04-28 15:10:02 -0700

    check

--- generate_param_policy.m
+++ generate_param_policy.m
@@ -3,5 +3,6 @@ function param_policy = generate_param_policy
     param_policy.e = 1;
     % percentage of people under policy control (could be work X age specific)
     % each entry is in (0, 1)
+    % assume (0.01, 0.999) following convention
     param_policy.policy_pct = [0.01 0.01 0.01; 0.01 0.01 0.01; 0.01 0.01 0.01];
 
