fac523af1aa0b040d2e364fac1d40fdbda3451e6
Mingxi Zhu <48236042+mingxiz@users.noreply.github.com>
2020-05-21 16:51:28 -0700

    Update generate_param_beta2.m

--- generate_param_beta2.m
+++ generate_param_beta2.m
@@ -1,7 +1,7 @@
 function parm_beta = generate_param_beta2(n_age_strat, n_work_strat, param_epi,W_multiplier, O_multiplier, S_multiplier, H_multipler, phaseIndicator)
 
 %here need table be on same dictionary
-    v_table_nopolicy =  getCM(W_multiplier, O_multiplier, S_multiplier, H_multipler, phaseIndicator);
+    v_table_nopolicy =  getCM(W_multiplier, O_multiplier, S_multiplier, H_multipler, 1, 1, phaseIndicator)
     
     % v_table_nopolicy(:,3) = 0.2*v_table_nopolicy(:,3);
     % v_table_nopolicy = 0.8*v_table_nopolicy;
