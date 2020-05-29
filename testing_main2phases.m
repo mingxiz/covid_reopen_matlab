8aeff2c3f40e482520cbd1abb87e3508c5b15be7
Mingxi Zhu <48236042+mingxiz@users.noreply.github.com>
2020-05-29 09:30:36 -0700

    Add files via upload

--- testing_main2phases.m
+++ testing_main2phases.m
@@ -1,33 +1,27 @@
-W_multiplier = 1;
-O_multiplier = 0.3;
-S_multiplier = 0;
-H_multiplier = 0.6;
+close all 
+W_multiplier =  0.7;
+O_multiplier = 1;
+S_multiplier = 1;
+H_multiplier = 0.5;
 
 t_SIP = 50;
 t_reopen = 110;
-t_end = 400;
-N_zero_patient = 10;
+t_end = 130;
+N_zero_patient = 4;
 
-W_multiplier_r = 1;
-O_multiplier_r = 1;
+W_multiplier_r = W_multiplier;
+O_multiplier_r = O_multiplier;
 S_multiplier_r = 1;
-H_multipler_r = 0.8;
+H_multipler_r = H_multiplier;
 
-infect_rate = 0.028;
+infect_rate = 0.03;
 options_figure = 1;
 
-fvpvnvRatioVec = [0 0 0.5]
-fvpvnvRatioVec_r = [0 0.5 1]
-epi_ymo_fvpvnv_fullTWTOTS_r =  [1.22
-4.466
-0.77
-2.21
-2.16
-2.65
-5.06
-0.432
-0.03
-2.52
-2.94
-3.555]
-output = main2_phases_2016_reopen(W_multiplier, O_multiplier, S_multiplier, H_multiplier,W_multiplier_r,O_multiplier_r,S_multiplier_r,H_multipler_r, fvpvnvRatioVec, fvpvnvRatioVec_r, epi_ymo_fvpvnv_fullTWTOTS_r, t_SIP, t_reopen, t_end, N_zero_patient, infect_rate, options_figure)
+fv_TWratio = 1
+pv_TWratio = 1
+nv_TWratio = 1
+TOratio = 1
+TSratio = 1
+o_specialCare = 1
+
+output = main2_phases_2016_reopen(W_multiplier, O_multiplier, S_multiplier, H_multiplier, W_multiplier_r,O_multiplier_r,S_multiplier_r,H_multipler_r, fv_TWratio, pv_TWratio, nv_TWratio, TOratio, TSratio, o_specialCare, t_SIP, t_reopen, t_end, N_zero_patient, infect_rate, options_figure)
